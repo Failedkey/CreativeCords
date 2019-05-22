fde61147271d36ad40b9a9adc12c9aca60903d61
Failedkey <keyfailed@gmail.com>
2019-05-22 15:25:05 -0700

    Added commands

--- CreativeCords/src/creativeCords/Startup.java
+++ CreativeCords/src/creativeCords/Startup.java
@@ -2,9 +2,16 @@ package creativeCords;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import creativeCords.Commands.CommandHelp;
+
 public class Startup extends JavaPlugin{
 
 	public void onEnable() {
+		//Commands
+		getCommand("help").setExecutor(new CommandHelp());
+		
+		//Events
+		
 		this.saveConfig();
 		super.onEnable();
 	}
