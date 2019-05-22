246cccbc2e7e12efdbff0ffbc8071eeea924d264
Failedkey <keyfailed@gmail.com>
2019-05-22 16:07:07 -0700

    MoveEvent

--- CreativeCords/src/creativeCords/Startup.java
+++ CreativeCords/src/creativeCords/Startup.java
@@ -3,12 +3,14 @@ package creativeCords;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import creativeCords.Commands.CommandHelp;
+import creativeCords.Commands.CommandPos;
 
 public class Startup extends JavaPlugin{
 	
 	public void onEnable() {
 		//Commands
 		getCommand("Chelp").setExecutor(new CommandHelp());
+		getCommand("pos").setExecutor(new CommandPos());
 		
 		//Events
 		
