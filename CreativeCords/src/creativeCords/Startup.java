db45457ad6e410d1916ad4edc310df3362abbb40
Failedkey <keyfailed@gmail.com>
2019-05-22 14:52:44 -0700

    FirstCommit

---
+++ CreativeCords/src/creativeCords/Startup.java
@@ -0,0 +1,15 @@
+package creativeCords;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Startup extends JavaPlugin{
+
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+}
