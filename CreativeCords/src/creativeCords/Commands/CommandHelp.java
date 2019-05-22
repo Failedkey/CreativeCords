421549556a840ff081cf5e21c93e7053d5ca6003
Failedkey <keyfailed@gmail.com>
2019-05-22 15:28:07 -0700

    test

--- CreativeCords/src/creativeCords/Commands/CommandHelp.java
+++ CreativeCords/src/creativeCords/Commands/CommandHelp.java
@@ -3,12 +3,15 @@ package creativeCords.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandHelp implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
-		// TODO Auto-generated method stub
+		if(arg0 instanceof Player) {
+			
+		}
 		return false;
 	}
 
